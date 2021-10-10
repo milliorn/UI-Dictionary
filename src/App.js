a9ea9661eed916d3959d7c7a648767d9b5499060
Scott Milliorn <scottmilliorn@gmail.com>
2021-10-09 21:40:29 -0700

    data

--- src/App.js
+++ src/App.js
@@ -5,57 +5,57 @@ import "./App.css";
 import Header from "./components/Header/Header";
 
 function App() {
-	const [word, setWord] = useState("");
-	const [meanings, setMeanings] = useState([]);
-	const [category, setCategory] = useState("en");
+    const [word, setWord] = useState("");
+    const [meanings, setMeanings] = useState([]);
+    const [category, setCategory] = useState("en");
 
-		const dictionaryApi = async () => {
-			try {
-				const data = await axios.get(
-					"https://api.dictionaryapi.dev/api/v2/entries/en/plane"
-				);
+        const dictionaryApi = async () => {
+            try {
+                const data = await axios.get(
+                    `https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`
+                );
 
-				setMeanings(data.data);
-			} catch (error) {
-				console.log(error);
-			}
-		};
+                setMeanings(data.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
 
-		console.log(meanings);
+        console.log(meanings);
 
-	useEffect(() => {
-		dictionaryApi();
-		// eslint-disable-next-line
-	}, [word, category]);
+    useEffect(() => {
+        dictionaryApi();
+        // eslint-disable-next-line
+    }, [word, category]);
 
-		return (
-			<div
-				className="App"
-				style={{
-					height: "100vh",
-					backgroundColor: "#282c34",
-					color: "white",
-				}}
-			>
-				<Container
-					maxWidth="md"
-					style={{
-						display: "flex",
-						flexDirection: "column",
-						height: "100vh",
-						justifyContent: "space-evenly",
-					}}
-				>
-					<Header
-						setWord={setWord}
-						category={category}
-						setCategory={setCategory}
-						word={word}
-						setMeanings={setMeanings}
-					/>
-				</Container>
-			</div>
-			);
+        return (
+            <div
+                className="App"
+                style={{
+                    height: "100vh",
+                    backgroundColor: "#282c34",
+                    color: "white",
+                }}
+            >
+                <Container
+                    maxWidth="md"
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        height: "100vh",
+                        justifyContent: "space-evenly",
+                    }}
+                >
+                    <Header
+                        setWord={setWord}
+                        category={category}
+                        setCategory={setCategory}
+                        word={word}
+                        setMeanings={setMeanings}
+                    />
+                </Container>
+            </div>
+            );
 }
 
 export default App;
