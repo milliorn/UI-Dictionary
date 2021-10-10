6953eac8b37f91dc9ace3be4e23379e821071f7d
Scott Milliorn <scottmilliorn@gmail.com>
2021-10-10 00:15:13 -0700

    light/dark mode

--- src/App.js
+++ src/App.js
@@ -1,61 +1,88 @@
-import { Container } from "@material-ui/core";
+import { Container, Switch, withStyles } from "@material-ui/core";
+import { grey } from "@material-ui/core/colors";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./App.css";
+import Definitions from "./components/Definitions/Definitions";
 import Header from "./components/Header/Header";
 
 function App() {
     const [word, setWord] = useState("");
     const [meanings, setMeanings] = useState([]);
     const [category, setCategory] = useState("en");
-
-        const dictionaryApi = async () => {
-            try {
-                const data = await axios.get(
-                    `https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`
-                );
-
-                setMeanings(data.data);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-
-        console.log(meanings);
+        const [LightMode, setLightMode] = useState(false);
+    const dictionaryApi = async () => {
+        try {
+            const data = await axios.get(
+                `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+            );
+            setMeanings(data.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     useEffect(() => {
         dictionaryApi();
         // eslint-disable-next-line
     }, [word, category]);
 
-        return (
-            <div
-                className="App"
+    const DarkMode = withStyles({
+        switchBase: {
+            color: grey[50],
+            "&$checked": {
+                color: grey[900],
+            },
+            "&$checked + $track": {
+                backgroundColor: grey[500],
+            },
+        },
+        checked: {},
+        track: {},
+        })(Switch);
+
+    return (
+        <div
+            className="App"
+            style={{
+                height: "100vh",
+                backgroundColor:LightMode?"#fff":"#282c34",
+                color: LightMode ? "black" : "white",
+                transition: "all 0.8s linear"
+            }}
+        >
+            <Container
+                maxWidth="md"
                 style={{
+                    display: "flex",
+                    flexDirection: "column",
                     height: "100vh",
-                    backgroundColor: "#282c34",
-                    color: "white",
+                    justifyContent: "space-evenly",
                 }}
             >
-                <Container
-                    maxWidth="md"
-                    style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        height: "100vh",
-                        justifyContent: "space-evenly",
-                    }}
-                >
-                    <Header
-                        setWord={setWord}
-                        category={category}
-                        setCategory={setCategory}
-                        word={word}
-                        setMeanings={setMeanings}
+                <div
+                    style={{ position: "absolute", top: 0, right: 15, paddingTop: 10 }}
+                        >
+                            <span>{LightMode?"Light":"Dark"}</span>
+                            <DarkMode
+                                checked={LightMode}
+                                onChange={() => setLightMode(!LightMode)}
                     />
-                </Container>
-            </div>
-            );
+                </div>
+                <Header
+                    setWord={setWord}
+                    category={category}
+                    setCategory={setCategory}
+                    word={word}
+                    setMeanings={setMeanings}
+                    LightMode={LightMode}
+                />
+                {meanings && (
+                    <Definitions word={word} meanings={meanings} category={category} />
+                )}
+            </Container>
+        </div>
+    );
 }
 
 export default App;
