188e6f44c4de1ea2b8b5e43ea73bd9c3b06e1c19
Scott Milliorn <scottmilliorn@gmail.com>
2021-10-14 00:49:37 -0700

    lighthouse opts

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,57 +1,59 @@
 import {
-	createTheme,
-	MenuItem,
-	TextField,
-	ThemeProvider,
+  createTheme,
+  MenuItem,
+  TextField,
+  ThemeProvider,
 } from "@material-ui/core";
 import React from "react";
 import "./Header.css";
 import categories from "../../data/category";
 
 const Header = ({ setCategory, category, word, setWord, LightMode }) => {
-	const darkTheme = createTheme({
-		palette: {
-			primary: {
-				main:LightMode?"#000" : "#fff",
-			},
-			type:LightMode? "light": "dark",
-		},
-	});
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: LightMode ? "#000" : "#fff",
+      },
+      type: LightMode ? "light" : "dark",
+    },
+  });
 
-	const handleChange = (language) => {
-		setCategory(language.target.value);
-        setWord("");
-        
-	}
+  const handleChange = (language) => {
+    setCategory(language.target.value);
+    setWord("");
+  };
 
-	return (
-		<div className="header">
-			<span className="title">{word ? word : "Word Lexicon"}</span>
-			<div className="inputs">
-				<ThemeProvider theme={darkTheme}>
-					<TextField
-						className="search"
-						label="Search a Word"
-						value={word}
-						onChange={(e) => setWord(e.target.value)}
-					/>
-					<TextField
-						select
-						label="Language"
-						value={category}
-						onChange={(e) => handleChange(e)}
-						className="select"
-					>
-						{categories.map((option) => (
-							<MenuItem key={option.label} value={option.label}>
-								{option.value}
-							</MenuItem>
-						))}
-					</TextField>
-				</ThemeProvider>
-			</div>
-		</div>
-	);
+  return (
+    <div className="header">
+      <span className="title">{word ? word : "Word Lexicon"}</span>
+      <div className="inputs">
+        <ThemeProvider theme={darkTheme}>
+          <TextField
+            type="text"
+            className="search"
+            aria-label="Search a Word"
+            aria-required="true"
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
+          />
+          <TextField
+            select
+            aria-label="Language"
+            aria-required="true"
+            value={category}
+            onChange={(e) => handleChange(e)}
+            className="select"
+          >
+            {categories.map((option) => (
+              <MenuItem key={option.label} value={option.label}>
+                {option.value}
+              </MenuItem>
+            ))}
+          </TextField>
+        </ThemeProvider>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
